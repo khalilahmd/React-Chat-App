b09907d533c7c3cc43327f8387921e7b16efc710
khalilahmd <khalilseikhu363@gmail.com>
2023-01-14 19:05:44 +0500

    initial commit with firebase config

--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase/compat/app';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAux5UCLlZ2opYTlehCD1evJgqB2btwAjQ",
+  authDomain: "chat-application-60b09.firebaseapp.com",
+  databaseURL: "https://chat-application-60b09-default-rtdb.firebaseio.com",
+  projectId: "chat-application-60b09",
+  storageBucket: "chat-application-60b09.appspot.com",
+  messagingSenderId: "818980315486",
+  appId: "1:818980315486:web:e23759989501431e446fcf",
+  measurementId: "G-70VBJVGPQS"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,6 +27,8 @@ root.render(
   </React.StrictMode>
 );
 
+
+// https://chat-application-60b09-default-rtdb.firebaseio.com/
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
